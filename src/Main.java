e094b89cd91b133a4fbddf7147b6ff3c28706228
Anna test <annaterekhov1@gmail.com>
2022-12-19 23:01:56 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 100;
+        int persent = 20;
+        boolean isRegisered = true;
+        int bonus = x / persent;
+
+
+        System.out.println(bonus);
+
+    }
+
+
+}
